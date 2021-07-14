b47ed09c87ecda4397595e4f9be632afed632bbe
Farrukh Bulbulov <80750954+fbulbulov@users.noreply.github.com>
2021-07-14 13:28:01 +0200

    logout route app

--- app.py
+++ app.py
@@ -25,19 +25,29 @@ class RegisterForm(FlaskForm):
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>'+form.username.data+' '+form.password.data + '</h1>'
     return render_template('login.html', form=form)
 
-@app.route('/singup')
+@app.route('/singup', methods=['GET', 'POST'])
 def singup():
     form=RegisterForm()
+    if form.validate_on_submit():
+        return '<h1>'+form.username.data+' '+form.email.data + ' ' + form.password.data + '</h1>'
     return render_template('singup.html', form=form)
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__=="__main__":
     app.run(debug=True)
