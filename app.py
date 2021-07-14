a432cbfc262ab5c1808cf7f3e9f3d216a2bbbd4d
Farrukh Bulbulov <80750954+fbulbulov@users.noreply.github.com>
2021-07-14 09:38:46 +0200

    importing the libraries

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtform import Stringfield, Passwordfield, BooleanField
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
-app=Flas(__name__)
+app=Flask(__name__)
 
 
 @app.route('/')
