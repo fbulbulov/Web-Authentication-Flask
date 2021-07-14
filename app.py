01a08f5da7d3ff4025b5233b386b9b75b902d16c
Farrukh Bulbulov <80750954+fbulbulov@users.noreply.github.com>
2021-07-14 12:44:57 +0200

    register form

--- app.py
+++ app.py
@@ -8,25 +8,34 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app=Flask(__name__)
+app.config['SECRET_KEY']="Secret_key"
 Bootstrap(app)
 
 class LoginForm(FlaskForm):
     username=StringField('username',validators=[InputRequired(), Length(min=3, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=20)])
+    remember= BooleanField('remember me')
 
+class RegisterForm(FlaskForm):
+    email=StringField('email', validators[InputRequired(), Email(message='invalid email', length=(max=30)])
+    username=StringField('username',validators=[InputRequired(), Length(min=3, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=20)])
+    
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 @app.route('/singup')
-def login():
+def singup():
     return render_template('singup.html')
 
 @app.route('/dashboard')
-def login():
+def dashboard():
     return render_template('dashboard.html')
 
 if __name__=="__main__":
