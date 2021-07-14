5529135040fb9de5401bca57f240b85634d7dcbb
Farrukh Bulbulov <80750954+fbulbulov@users.noreply.github.com>
2021-07-14 20:28:09 +0200

    updates

--- app.py
+++ app.py
@@ -6,31 +6,38 @@ from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_sqlalchemy import SQLAlchemy 
 
 
+app = Flask(__name__, template_folder='templates') 
 
-app=Flask(__name__)
-app.config['SECRET_KEY']="Secret_key"
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////Users/fb/Desktop/StriveAI/Web-Authentication-Flask/database.db'
-Bootstrap(app)
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/antho/Documents/login-example/database.db'
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class user(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(15), unique=True)
-    email=db.Column(db.String(40), unique=True)
-    password=db.Column(db.String(20))
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
-    username=StringField('username',validators=[InputRequired(), Length(min=3, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=20)])
-    remember= BooleanField('remember me')
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
-    email=StringField('username', Email(message='Invalid email'), Length(max=40))
-    username=StringField('username',validators=[InputRequired(), Length(min=3, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=20)])
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 @app.route('/')
 def index():
@@ -39,20 +46,38 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
-        return '<h1>'+form.username.data+' '+form.password.data + '</h1>'
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
     return render_template('login.html', form=form)
 
-@app.route('/singup', methods=['GET', 'POST'])
-def singup():
-    form=RegisterForm()
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
     if form.validate_on_submit():
-        return '<h1>'+form.username.data+' '+form.email.data + ' ' + form.password.data + '</h1>'
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
     return render_template('singup.html', form=form)
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', name=current_user.username)
 
 @app.route('/logout')
 @login_required
@@ -60,5 +85,5 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-if __name__=="__main__":
+if __name__ == '__main__':
     app.run(debug=True)
